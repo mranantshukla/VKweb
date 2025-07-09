9236c53a07e42ee3563d11f3b6842175278d2949
mranantshukla <mr.anantshukla@gmail.com>
2025-07-09 17:38:34 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/your-repo-name/', // 👈 very important
+  base: '/VKweb/', // 👈 very important
   plugins: [react()],
 });
-
