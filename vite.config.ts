be14bd86daec8722194593ed90f09a637d1ba984
mranantshukla <mr.anantshukla@gmail.com>
2025-07-09 17:58:15 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/VKweb/', // 👈 very important
+  base: '/VKweb/', // 👈 this must match your repo name exactly
   plugins: [react()],
-});
+})
